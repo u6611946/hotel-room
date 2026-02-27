a371d4b6c61d8350e91097e4c8fd7139f7ad7eca
Phyo Yadanar Min <u6611946@au.edu>
2026-02-28 01:22:47 +0700

    fix: update next config and seed images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 };
-
 export default nextConfig;
